02f40b5797d41b51943bc8fed8a63d689e340086
Venkatasai51 <112601035+Venkatasai51@users.noreply.github.com>
2022-09-07 11:05:18 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+	int i,j,rows;
+	std::cin>>rows;
+	for(i=1;i<=rows;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			std::cout<<"sai";
+		}
+		std::cout<<"\n";
+	}
+	return 0;
+}
